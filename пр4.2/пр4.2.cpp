aa2d3d43aa6a18cba65b2d4a57eb3364deb7eddc
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-10 17:01:28 +0300

    Add project files.

---
+++ пр4.2/пр4.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A,B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A=tan(x) + 1.;
+
+		if (x <= -5)
+			B = exp(x / fabs(1 - x));
+		else
+			if (x <= 3 && x > -5)
+				B = 3.2 + log10(1.8 * x * x);
+			else
+				B = fabs(x) * sqrt(x);
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
